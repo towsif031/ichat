7d5dff33e638fa56be697e029a8ad2d68f037348
Towsif Ahmed Omi <towsif031@gmail.com>
2018-10-03 21:28:19 +0600

    Added java classes.

---
+++ app/src/main/java/com/example/towsif/ichat/CodelabPreferences.java
@@ -0,0 +1,7 @@
+package com.example.towsif.ichat;
+
+public class CodelabPreferences {
+
+    public static final String FRIENDLY_MSG_LENGTH = "friendly_msg_length";
+
+}
---
+++ app/src/main/java/com/example/towsif/ichat/MyFirebaseInstanceIdService.java
@@ -0,0 +1,11 @@
+package com.example.towsif.ichat;
+
+public class MyFirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+    private static final String FRIENDLY_ENGAGE_TOPIC = "friendly_engage";
+
+    public void onTokenRefresh() {
+    }
+
+}
